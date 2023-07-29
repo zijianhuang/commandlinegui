a4e97325a1fd44ad6e5d01920fb64ed118860fd1
fonlow <zijianhuang2000@gmail.com>
2023-07-30 09:32:37 +1000

    ok

---
+++ RobocopyParametersCore/FlagsEditors.cs
@@ -0,0 +1,12 @@
+﻿using Fonlow.CommandLineGui.Gui;
+
+namespace Fonlow.CommandLineGui.Robocopy
+{
+    public class CopyFlagsEditor : FlagsEditor<FlagsUC<CopyFlags>, CopyFlags>
+    {
+    }
+
+    public class RashFlagsEditor : FlagsEditor<FlagsUC<Rashcneto>, Rashcneto>
+    {
+    }
+}
---
+++ RobocopyParametersCore/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Error List, point to "Suppress Message(s)", and click 
+// "In Project Suppression File".
+// You do not need to add suppressions to this file manually.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1714:FlagsEnumsShouldHavePluralNames", Scope = "type", Target = "Fonlow.CommandLineGui.Robocopy.Rashcneto")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1703:ResourceStringsShouldBeSpelledCorrectly", MessageId = "Dest", Scope = "resource", Target = "Fonlow.CommandLineGui.Robocopy.Properties.Resources.resources")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Scope = "member", Target = "Fonlow.CommandLineGui.Robocopy.RobocopyOptions.#Files")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Scope = "member", Target = "Fonlow.CommandLineGui.Robocopy.RobocopyOptions.#SlashXf")]
