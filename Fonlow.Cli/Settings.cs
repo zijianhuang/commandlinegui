2e6fab14e4436414e851664e5185698b67b241b0
Zijian <zijianhuang2000@gmail.com>
2025-02-22 09:15:35 +1000

    ok

---
+++ Fonlow.Cli/Settings.cs
@@ -0,0 +1,24 @@
+﻿namespace Fonlow.Cli
+{
+	public sealed class Settings
+	{
+		public string AssemblyNameOfCommand { get; set; } = "RobocopyParameters";
+
+		public string[] AssemblyNames { get; set; }
+
+		public PluginAllocationMethod PluginAllocationMethod { get; set; } = PluginAllocationMethod.Mono;
+
+	}
+
+	/// <summary>
+	/// Mono: locate only one assembly in property AssemblyNameOfCommand,
+	/// Registration: plugin files in the same directory of commandLineGui.exe.
+	/// </summary>
+	public enum PluginAllocationMethod
+	{
+		Mono, 
+		Registration
+	};
+
+
+}
